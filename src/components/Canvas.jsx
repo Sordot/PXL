3db0b017071d34a270348e3f253ee6e3e9b18202
Sordot <tgevirtz@gmail.com>
2022-04-04 16:46:36 -0500

    adds export functionality

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,21 +1,23 @@
-import React from "react";
+import React, {useRef} from "react";
 import Row from "./Row.jsx"
+import { exportComponentAsPNG } from "react-component-export-image";
 import "../styles/canvas.scss"
 
 const Canvas = ({width, height, selectedColor}) => {
 
+    const panelRef = useRef()
+
     let rows = [];
 
-    //add the appropriate amount of Row components to rows based off of the height (the canvas will always be a square)
+    //add the appropriate amount of Row components to rows based off of the height
     for(let i = 0; i < height; i++) {
         rows.push(<Row key={i} width={width} selectedColor={selectedColor} />)
     }
 
     return <>
         <div id='canvas'>
-            <div id='pixels'>
-                {rows}
-            </div>
+            <div id='pixels' ref={panelRef}>{rows}</div>
+            <button className="button" onClick={() => exportComponentAsPNG(panelRef)}>Export as PNG</button>
         </div>
     </>
 
