fd407aa9cf9b7355d97ef01035c5a44a1354f732
Sordot <tgevirtz@gmail.com>
2022-04-04 16:53:16 -0500

    basic functionality complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./styles/App.scss"
 import Editor from "./components/Editor.jsx"
 
+//TODO option to change color palette (use the 'colors' prop on CirclePicker)
+//TODO drag drawing
+//TODO eraser
+
 function App() {
   return <>
     <div className="App">
